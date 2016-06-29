e3706e69604fcb34180f3977d9978b26fcde48fb
Romeu Rosa <romeu.rosa@noesis.pt>
2016-06-29 17:57:26 +0100

    Removed non-used methods from the Golang API to not cause confusion.

--- go/src/github.com/romeufcrosa/http-server/server.go
+++ go/src/github.com/romeufcrosa/http-server/server.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-    "encoding/json"
-    "io/ioutil"
+    //"encoding/json"
+    //"io/ioutil"
     "log"
     "fmt"
     "net/http"
@@ -10,45 +10,20 @@ import (
     "strconv"
 )
 
-type postStruct struct {
-    Name string
-}
-
-func userHandler(w http.ResponseWriter, r *http.Request) {
+func creditHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Println(r.Method)
     switch r.Method {
         case "GET":
             // Serve the resource
-            userID, err := strconv.Atoi(r.URL.Path[len("/credits/"):])
+            creditID, err := strconv.Atoi(r.URL.Path[len("/credits/"):])
             if err != nil {
                 log.Fatal(err)
             }
-            //fmt.Printf("Retrieving info for user = %d\n", userID)
-            read(userID)
+            read(creditID)
         case "POST":
             // Create a new record
-            var p postStruct
-            decoder := json.NewDecoder(r.Body)
-            err := decoder.Decode(&p)
-            if err != nil {
-                log.Fatal(err)
-            }
-            createdID := create(p.Name)
-            log.Printf("Inserted row ID = %d", createdID)
         case "PUT":
             // Update an existing record
-            var f interface{}
-            body, _ := ioutil.ReadAll(r.Body)
-            err := json.Unmarshal(body, &f)
-            if err != nil {
-                log.Fatal(err)
-            }
-            m := f.(map[string]interface{})
-            var newName = m["name"].(string)
-            var updatedID = int(m["id"].(float64))
-            read(updatedID)
-            update(updatedID, newName)
-            read(updatedID)
         case "DELETE":
             // Remove the record
         default:
@@ -57,6 +32,6 @@ func userHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-    http.HandleFunc("/credits/", userHandler)
+    http.HandleFunc("/credits/", creditHandler)
     http.ListenAndServe(":8080", nil)
 }
